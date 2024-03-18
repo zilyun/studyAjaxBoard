57b9a9e40a93a43a99119c4565f660d8a86f5335
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-03-18 14:16:17 +0900

    Feat: 게시판 만들기2

--- src/main/java/net/board/db/BoardDAO.java
+++ src/main/java/net/board/db/BoardDAO.java
@@ -1,17 +1,111 @@
 package net.board.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+// SQL과 관련된 객체와 List 객체, JNDI 관련 객체를 사용하기 위해 import 합니다.
 public class BoardDAO {
 
+	private DataSource ds;
+	
+	// 생성자에서 JNDI 리소스를 참조하여 Connection 객체를 얻어옵니다.
+	public BoardDAO() {
+		try {
+			Context init = new InitialContext();
+			ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
+		} catch (Exception ex) {
+			System.out.println("DB 연결 실패 : " + ex);
+		}
+	}
+	
+	// 글의 갯수 구하기 
 	public int getListCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		String sql = "select count(*) from board";
+		int x = 0;
+		try (Connection con = ds.getConnection(); 
+			PreparedStatement pstmt = con.prepareStatement(sql);) {
+			
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					x = rs.getInt(1);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("getListCount() 에러: " + ex);
+		}
+		return x;
+	} // getListCount() end
 
+	// 글 목록 보기
 	public List<BoardBean> getBoardList(int page, int limit) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+		
+		// page : 페이지
+		// limit : 페이지 당 목록의 수
+		// board_re_ref desc, board_re_seq asc에 의해 정렬한 것을 
+		// 조건절에 맞는 rnum의 범위 만큼 가져오는 쿼리문입니다.
+		
+		String board_list_sql = 
+				"select * "
+			  + " from (select rownum rnum, j.* "
+			  + "		from (select board.*, nvl(cnt,0) cnt "
+			  + "			  from board left outer join (select comment_board_num,count(*) cnt "
+			  + "										  from comm"
+			  + "										  group by comment_board_num)"
+			  + "			  on board_num=comment_board_num"
+			  + "			  order by BOARD_RE_REF desc,"
+			  + "			  BOARD_RE_SEQ asc) j "
+			  + "		where rownum <= ? "
+			  + "		) "		
+			  + " where rnum>=? and rnum<=?";
+		
+		List<BoardBean> list = new ArrayList<BoardBean>();
+		// 한 페이지당 10개씩 목록인 경우 1페이지, 2페이지, 3페이지, 4페이지 ...
+		int startrow = (page - 1) * limit + 1; //  읽기 시작할 row 번호(1  11  21  31 ...)
+		int endrow  = startrow + limit - 1;     // 읽을 마지막 row 번호(10 20  30  40 ...)
+		
+		try (Connection con = ds.getConnection();
+			 PreparedStatement pstmt = con.prepareStatement(board_list_sql);) {
+				pstmt.setInt(1, endrow);
+				pstmt.setInt(2, startrow);
+				pstmt.setInt(3, endrow);
+			
+				try (ResultSet rs = pstmt.executeQuery()) {
+					
+					// DB에서 가져온 데이터를 BoardBean에 담습니다.
+					while (rs.next()) {
+						BoardBean board = new BoardBean();
+						board.setBoard_num(rs.getInt("BOARD_NUM"));
+						board.setBoard_name(rs.getString("BOARD_NAME"));
+						board.setBoard_subject(rs.getString("BOARD_SUBJECT"));
+						board.setBoard_content(rs.getString("BOARD_CONTENT"));
+						board.setBoard_file(rs.getString("BoARD_FILE"));
+						board.setBoard_re_ref(rs.getInt("BOARD_RE_REF"));
+						board.setBoard_re_lev(rs.getInt("BOARD_RE_LEV"));
+						board.setBoard_re_seq(rs.getInt("BOARD_RE_SEQ"));
+						board.setBoard_readcount(rs.getInt("BOARD_READCOUNT"));
+						board.setBoard_date(rs.getString("BOARD_DATE"));
+						board.setCnt(rs.getInt("cnt"));
+						list.add(board); // 값을 담은 객체를 리스트에 저장힙니다.
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+  				
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("getBoardList() 에러 : " + ex);
+		}
+		return list;
+	} // getBoardList() 메서드 end
 }
--- src/main/webapp/board/boardlist.jsp
+++ src/main/webapp/board/boardlist.jsp
@@ -1,12 +1,101 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>게시판</title>
+	<jsp:include page="header.jsp" />
+	<style>
+	select.form-control {
+		width: auto;
+		margin-bottom: 2em;
+		display: inline-block;
+	}
+	
+	.rows {
+		text-align: right;
+	}
+	
+	.gray {
+		color: gray;
+	}
+	
+	body > div > table > thead > tr:nth-child(2) > th:nth-child(1) {width:8%}
+	body > div > table > thead > tr:nth-child(2) > th:nth-child(1) {width:50%}
+	body > div > table > thead > tr:nth-child(2) > th:nth-child(1) {width:14%}
+	body > div > table > thead > tr:nth-child(2) > th:nth-child(1) {width:17%}
+	body > div > table > thead > tr:nth-child(2) > th:nth-child(1) {width:11%}
+	</style>
+	<script src="${pageContext.request.contextPath}/js/list.js"></script>
+	<title>MVC 게시판</title>
 </head>
 <body>
-
+<div class="container">
+	<%-- 게시글이 있는 경우 --%>
+	<c:if test="${listcount > 0}">
+		<div class="rows">
+			<span>줄보기</span>
+			<select class="form-control" id="viewcount">
+				<option value="1">1</option>
+				<option value="3">3</option>
+				<option value="5">5</option>
+				<option value="7">7</option>
+				<option value="10" selected>10</option>
+			</select>
+		</div>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th colspan="3">MVC 게시판 - list</th>
+					<th colspan="2">
+						<span>글 개수 : ${listcount}</span>
+					</th>
+				</tr>
+				<tr>
+					<th><div>번호</div></th>
+					<th><div>제목</div></th>
+					<th><div>작성자</div></th>
+					<th><div>날짜</div></th>
+					<th><div>조회수</div></th>
+				</tr>
+			</thead>
+			<tbody>
+			<c:set var="num" value="${listcount - (page-1)*limit}" />
+			<c:forEach var="b" items="${boardlist}">
+				<tr>
+					<td> <%-- 번호 --%>
+						<c:out value="${num}" /> <%-- num 출력 --%>
+						<c:set var="num" value="${num-1}" /> <%-- num=num-1; 의미 --%>
+					</td>
+					<td> <%-- 제목 --%>
+						<div>
+							<%-- 답변글 제목 앞에 여백 처리 부분 
+								 board_re_lev, board_num, 
+								 board_subject, board_name, board_date,
+								 board_readcount : property 이름 --%>
+							<c:if test="${b.board_re_lev != 0}"> <%-- 답글인 경우 --%>
+								<c:forEach var="a" begin="0" end="${b.board_re_lev*2}" step="1">
+								&nbsp;
+								</c:forEach>
+								<img src='${pageContext.request.contextPath}/image/line.gif'>
+							</c:if>
+						</div>
+					</td>
+				</tr>
+			</c:forEach>
+			</tbody>
+		</table>
+	</c:if>
+</div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
