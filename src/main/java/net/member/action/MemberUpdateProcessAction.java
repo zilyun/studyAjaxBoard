71da9351e65a1eceba29633d3342f853d80af567
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-04-02 12:30:24 +0900

    Rename: 파일 위치 수정

--- src/main/java/net/member/action/MemberUpdateProcessAction.java
+++ src/main/java/net/member/action/MemberUpdateProcessAction.java
@@ -68,7 +68,7 @@ public class MemberUpdateProcessAction implements Action {
 				out.println("alert('수정되었습니다.');");
 				out.println("location.href='BoardList.bo';");
 			} else {
-				out.println("alert('회우너 정보 수정에 실패했습니다.');");
+				out.println("alert('회원 정보 수정에 실패했습니다.');");
 				out.println("history.back()"); // 비밀번호를 제외한 다른 데이터는 유지 되어 있습니다.
 			}
 			out.println("</script>");
